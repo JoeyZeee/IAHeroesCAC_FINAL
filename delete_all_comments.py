2dcc08c56911043bdd51aa69344664b651285d0c
JoeyZ <76706398+JoeyZeee@users.noreply.github.com>
2026-03-22 18:49:29 -0500

    Update delete_all_comments.py

--- delete_all_comments.py
+++ delete_all_comments.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Path to your service account key JSON
-SERVICE_ACCOUNT_PATH = ''
+SERVICE_ACCOUNT_PATH = r''
 
     if not firebase_admin._apps:
         cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
