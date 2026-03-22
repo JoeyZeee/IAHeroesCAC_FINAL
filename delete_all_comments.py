8bfc1e799c2dc8461d5ed801b9fb35a25d8b7428
JoeyZ <76706398+JoeyZeee@users.noreply.github.com>
2026-03-22 18:49:05 -0500

    Update delete_all_comments.py

--- delete_all_comments.py
+++ delete_all_comments.py
@@ -2,9 +2,8 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Path to your service account key JSON
-SERVICE_ACCOUNT_PATH = r'C:\Users\jzamb\IAHeroesCAC_FINAL\IAHeroesCAC_FINAL\frontend\iaheroesv2-firebase-adminsdk-fbsvc-afa7db3fdb.json'
+SERVICE_ACCOUNT_PATH = ''
 
-def initialize_firebase():
     if not firebase_admin._apps:
         cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
         firebase_admin.initialize_app(cred)
